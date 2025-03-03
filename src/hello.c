0ce2d5988337ee6e12dda76b0c42de3839128d10
Placeless <mikoprpr@hotmail.com>
2025-03-03 16:23:48 +0900

    day1 modification & day2 skip & day3: scanf(list1-2.c) & day4 skip

--- src/hello.c
+++ src/hello.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main(int argc, char** argv){
-  printf("Hello World!\n");
-  printf("123\n456\n789\n");
-  printf("シングルクォーテーション:\'ダブルクォーテーション:\"\n");
+  printf("Hello World!\n123\n456\n789\nシングルクォーテーション:\'ダブルクォーテーション:\"\n");
+  printf("");
+  printf("");
   printf("私の名前は%sです。年齢は%d歳です。\n", "山田太郎", 20);
 	printf("イニシャルは、%cです。\n", 'Y');
 	printf("%lf + %lf = %lf\n", 1.2, 2.7, 1.2 + 2.7);
--- src/list1-2.c
+++ src/list1-2.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
 int main(int argc, char** argv){
+  int a;
+  printf("数値を入力: ");
+  scanf("%d",&a);
+  if (a >0)
+  {
+    printf("入力した値は正の数です．\n");
+  }
+  else
+  {
+    printf("入力した値は負の数です．\n");
+  }
   
   return 0;
 }
