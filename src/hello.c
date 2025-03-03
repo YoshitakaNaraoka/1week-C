8d1b228e0805fcf4ca01d83805270d28eba24dc7
Placeless <mikoprpr@hotmail.com>
2025-03-03 16:08:46 +0900

    day1 unification -> hello.c

--- src/hello.c
+++ src/hello.c
@@ -2,5 +2,12 @@
 
 int main(int argc, char** argv){
   printf("Hello World!\n");
+  printf("123\n456\n789\n");
+  printf("シングルクォーテーション:\'ダブルクォーテーション:\"\n");
+  printf("私の名前は%sです。年齢は%d歳です。\n", "山田太郎", 20);
+	printf("イニシャルは、%cです。\n", 'Y');
+	printf("%lf + %lf = %lf\n", 1.2, 2.7, 1.2 + 2.7);
+  printf("ABC");
+  printf("DEF\n");
   return 0;
 }
--- src/list1-4.c
+++ src/list1-4.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 int main(int argc, char** argv){
-    printf("ABC");
-    printf("DEF\n");
+    
     return 0;
 }
